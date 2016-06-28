ab6121a9dab16f3950cee8b7091ccd47114af471
Naftal Timotheus <naftal@efinity.com.na>
2016-06-28 12:44:46 +0100

    Begining of something great

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>DATA</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <script src="https://use.fontawesome.com/19d2508637.js"></script>
+
+        <style>
+            html, body {
+                height: 100%;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: 'Lato';
+                color: #fff;
+
+
+
+                background: #00c6ff; /* fallback for old browsers */
+                background: -webkit-linear-gradient(to left, #00c6ff , #0072ff); /* Chrome 10-25, Safari 5.1-6 */
+                background: linear-gradient(to left, #00c6ff , #0072ff); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+
+
+
+            }
+
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+
+            .title {
+                font-size: 150px;
+            }.sub-title {
+                font-size: 60px;
+            }
+            .big-icon{
+                font-size: 200px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="big-icon"><i class="fa fa-database"></i></div>
+                <div class="title">PUBLIC DATA</div>
+                <div class="sub-title">Public data must be public and eassly accessible</div>
+            </div>
+        </div>
+    </body>
+</html>
